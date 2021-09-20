465993dd98ccf534989e49d95cdc1691ee65b5a8
Mustapha Alaba <alabamustapha@gmail.com>
2021-09-20 22:24:12 +0100

    Change the setup

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {config: './config.js' },
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#ff0000'
+      }
+    },
   },
   variants: {
     extend: {},
