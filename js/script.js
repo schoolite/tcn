67e043c5cf54e02f2c2c2fc6225d70fd4fd06190
Mustapha Alaba <alabamustapha@gmail.com>
2021-09-23 01:27:21 +0100

    implement table dropdown menu

--- js/script.js
+++ js/script.js
@@ -43,44 +43,25 @@
             addToggler(navMenu, navCaret)
         });
 
+        const trOptionBtns = document.querySelectorAll('.tr-option-btn');
+        trOptionBtns.forEach(trOptionBtn => {
+            trOptionBtn.addEventListener('click', function(btn){
+                console.log(btn.currentTarget);
+                toggleOption(btn.currentTarget);
 
-
-// Dropdown
-
-// const dropDown = document.querySelectorAll('.dropdownList');
-
-// dropDown.forEach( el => {
-//     el.addEventListener('click', (e) => {
-//         let target = e.currentTarget;
-//         let childDom = target.children[1];
-
-//         childDom.innerHTML = ' ';
-
-//         let templateString = `   <div class="z-50 bg-white absolute w-24 -left-4 top-6">
-//                                     <ul class="list border rounded-lg text-xs font-medium">
-//                                         <li class="p-2 border-b cursor-pointer"><a href="#"> Edit</a></li>
-//                                         <li  class="p-2 border-b cursor-pointer"><a href="#"> View Region </a></li>
-//                                         <li  class="p-2 border-b cursor-pointer"><a href="#"> View Discos </a></li>
-//                                         <li  class="p-2 border-b cursor-pointer"><a href="#"> Change Status</a></li>
-//                                         <li  class="p-2 cursor-pointer"><a href="#"> Audit Trail</a></li>
-//                                     </ul> 
-//                                 </div>`;
-
-//     });
-// });
+            })
+        });
+        
+                
 
 });
 
-function options(ele){
-    
-   
-    if(ele.classList.contains('show-class')){
-        const trOption = document.querySelector( '.tr-option-btn.show-class .tr-options');
-        if(trOption){
-            trOption.remove();
-        }
-    }else{
-        let templateString = `<div class="tr-options z-50 bg-white absolute w-24 -left-4 top-6">
+function toggleOption(ele){
+            
+    const trOptions = document.querySelectorAll('.tr-options');
+    trOptions.forEach(trOptionEle => trOptionEle.remove());
+
+    let templateString = `<div class="tr-options z-50 bg-white absolute w-24 -left-4 top-6">
                             <ul class="list border rounded-lg text-xs font-medium">
                                 <li class="p-2 border-b cursor-pointer"><a href="#"> Edit</a></li>
                                 <li  class="p-2 border-b cursor-pointer"><a href="#"> View Region </a></li>
@@ -89,12 +70,15 @@ function options(ele){
                                 <li  class="p-2 cursor-pointer"><a href="#"> Audit Trail</a></li>
                             </ul> 
                         </div>`;
-        ele.insertAdjacentHTML('beforebegin', templateString); 
+    
+    if(ele.classList.contains('show-option')){
+        ele.classList.remove('show-option'); 
+        if(ele.nextSibling){
+            ele.nextSibling.remove();
+        }
+    }else{
+        ele.insertAdjacentHTML('afterend', templateString); 
         ele.classList.add('show-option'); 
     }
 
 }
-
-
-    
-    
