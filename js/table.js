8f3dce1db43f5b54d7f9530a8770613fbc0a4549
Adekeye Adeniyi <adekeyeimmanuel@gmail.com>
2022-01-24 17:20:30 +0100

    added view and edit button, upadate javasript functions and html table data

--- js/table.js
+++ js/table.js
@@ -1,13 +1,62 @@
 
 window.addEventListener('load', (e)=> {
-    let cells = document.querySelectorAll('.cell');
-    let activeCell;
+    let editButton = document.querySelector('#edit');
+    let viewButton = document.querySelector('#view');
 
-    cells.forEach(cell => {
-        cell.addEventListener('click', (e)=>{
-            activeCell = e;
-            
-            console.log(activeCell);
+    let viewCells = document.querySelectorAll('.view'); 
+    let editCells = document.querySelectorAll('.edit');
+
+
+    editButton.addEventListener('click', (e)=>{
+    
+        viewCells.forEach( cell =>{
+            cell.classList.remove('active');
         });
-    })
+ 
+        editCells.forEach( cell=>{
+             cell.classList.add('active');
+        });
+
+        getViewValue();
+    });
+
+    viewButton.addEventListener('click', (e)=>{
+
+        viewCells.forEach( cell =>{
+            cell.classList.add('active');
+        });
+ 
+        editCells.forEach( cell=>{
+             cell.classList.remove('active');
+        });
+
+        getEditValue();
+
+    });
+
+
+    function getEditValue(){
+        editCells.forEach( (cell, index) =>{
+            let value = cell.value;
+
+            updateViewCell(value, index);
+        })
+    }
+
+    function updateViewCell(value, index){
+        viewCells[index].textContent = value;
+    }
+
+    function getViewValue(){
+        viewCells.forEach((cell, index) => {
+            let value = cell.textContent;
+
+            updateEditCell(value, index);
+        })
+    }
+
+    function updateEditCell(value, index){
+        editCells[index].value = value;
+    }
+
 });
